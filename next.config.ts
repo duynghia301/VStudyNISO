ef6216b2c6b274a4d67ba5dd845c9fdeb5e63bc3
Nghia <144611826+duynghia301@users.noreply.github.com>
2025-09-12 12:59:15 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
